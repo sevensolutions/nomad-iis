f6dd03b8ae53a3ac95cb2beea45a0cac7ecbd9f5
Daniel Peinhopf <84123899+sevensolutions@users.noreply.github.com>
2023-07-23 09:22:21 +0200

    Update badges in Readme

--- src/NomadIIS/PluginInfo.cs
+++ src/NomadIIS/PluginInfo.cs
@@ -3,5 +3,5 @@
 internal static class PluginInfo
 {
 	public static readonly string Name = "iis";
-	public static readonly string Version = "0.1.0"; // Don't forget to update README.md
+	public static readonly string Version = "0.1.0";
 }
