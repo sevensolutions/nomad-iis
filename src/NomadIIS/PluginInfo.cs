d583bb9ec364e61639a0326b3942b923ff482d7f
Daniel Peinhopf <84123899+sevensolutions@users.noreply.github.com>
2023-10-14 16:05:03 +0200

    Release v0.4.0

--- src/NomadIIS/PluginInfo.cs
+++ src/NomadIIS/PluginInfo.cs
@@ -3,5 +3,5 @@
 internal static class PluginInfo
 {
 	public static readonly string Name = "iis";
-	public static readonly string Version = "0.3.0";
+	public static readonly string Version = "0.4.0";
 }
