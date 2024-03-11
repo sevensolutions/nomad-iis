3aeccf3584a3d6f403c237ca524ced1d2e707daf
Daniel Peinhopf <84123899+sevensolutions@users.noreply.github.com>
2024-03-11 10:11:30 +0100

    Update nomad to v1.7.5 + update protos.

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,4 @@
-$NomadVersion = "1.7.2"
+$NomadVersion = "1.7.5"
 $NomadDownloadUrl = "https://releases.hashicorp.com/nomad/${NomadVersion}/nomad_${NomadVersion}_windows_amd64.zip"
 $NomadZipFileLocation = ".\nomad\nomad_${NomadVersion}_windows_amd64.zip"
 
