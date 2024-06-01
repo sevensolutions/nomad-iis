1f12f6e74f893cbc1ff0d6d11d748371cc8d39f3
Daniel Peinhopf <84123899+sevensolutions@users.noreply.github.com>
2024-06-01 11:23:32 +0200

    #52: Add ASP.Net Sample App

---
+++ examples/AspNetSampleApp/AspNetSampleApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetSampleApp.MvcApplication" Language="C#" %>
---
+++ examples/AspNetSampleApp/AspNetSampleApp/Models/HomeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetSampleApp.Models
+{
+	public class HomeModel
+	{
+		public string PermissionCheckResult { get; set; }
+		public string EnvironmentVariables { get; set; }
+	}
+}
