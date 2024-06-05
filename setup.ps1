9c93a080e73b7724b3abdee824e62a72c1f55d1b
Daniel Peinhopf <84123899+sevensolutions@users.noreply.github.com>
2024-06-05 20:15:29 +0200

    Update to Nomad 1.8.0 to check compatibility.

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,4 @@
-$NomadVersion = "1.7.5"
+$NomadVersion = "1.8.0"
 $NomadDownloadUrl = "https://releases.hashicorp.com/nomad/${NomadVersion}/nomad_${NomadVersion}_windows_amd64.zip"
 $NomadZipFileLocation = ".\nomad\nomad_${NomadVersion}_windows_amd64.zip"
 
