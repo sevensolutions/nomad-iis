76f01018fd162a0426307b0fe7c5744a6be41862
sevensolutions <84123899+sevensolutions@users.noreply.github.com>
2024-08-18 17:55:05 +0200

    Set trailingslash config.

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -19,6 +19,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sevensolutions', // Usually your GitHub org/user name.
   projectName: 'nomad-iis', // Usually your repo name.
+	trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
