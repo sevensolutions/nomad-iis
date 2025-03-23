57ae9aa58ce1312ca264e9da748cd3e353b70ab8
Daniel Peinhopf <84123899+sevensolutions@users.noreply.github.com>
2025-03-23 10:53:12 +0100

    Debug endpoint and JWT authentication (#104)
    * #102: First draft of a debug API endpoint.
    
    * #102: Fix JWT auth and add token capabilities

--- src/NomadIIS/ManagementApi/Authorization.cs
+++ src/NomadIIS/ManagementApi/Authorization.cs
@@ -7,17 +7,43 @@ namespace NomadIIS.ManagementApi;
 
 public static class Authorization
 {
-	public static bool IsAuthorized ( this IisTaskHandle taskHandle, HttpContext httpContext )
+	public static bool IsAuthorized ( this IisTaskHandle taskHandle, HttpContext httpContext, AuthorizationCapability capability )
 	{
+		var jobNamespace = taskHandle.TaskConfig?.Namespace ?? throw new InvalidOperationException();
 		var jobName = taskHandle.TaskConfig?.JobName ?? throw new InvalidOperationException();
 		var allocId = taskHandle.TaskConfig?.AllocId ?? throw new InvalidOperationException();
 
-		if ( !httpContext.User.HasClaim( "job", jobName ) && !httpContext.User.HasClaim( "job", "*" ) )
+
+		if ( !httpContext.User.HasClaim( "namespace", jobName ) && !httpContext.User.HasClaim( "namespace", "*" ) )
+			return false;
+		if ( !httpContext.User.HasClaim( "jobName", jobName ) && !httpContext.User.HasClaim( "jobName", "*" ) )
 			return false;
 		if ( !httpContext.User.HasClaim( "allocId", allocId ) && !httpContext.User.HasClaim( "allocId", "*" ) )
 			return false;
 
+		if ( !IsAuthorized( httpContext, capability ) )
+			return false;
+
 		return true;
 	}
+	public static bool IsAuthorized ( HttpContext httpContext, AuthorizationCapability capability )
+	{
+		var capabilityName = Enum.GetName( capability )!;
+
+		if ( !httpContext.User.HasClaim( "capabilities", capabilityName ) && !httpContext.User.HasClaim( "capabilities", "*" ) )
+			return false;
+
+		return true;
+	}
+}
+
+public enum AuthorizationCapability
+{
+	Status,
+	FilesystemAccess,
+	AppPoolLifecycle,
+	Screenshots,
+	ProcDump,
+	Debug
 }
 #endif
