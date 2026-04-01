ab40f19582db04c1f936c0bad9f9846fba4f4fa5
sevensolutions <84123899+sevensolutions@users.noreply.github.com>
2026-04-01 16:54:54 +0200

    chore: bump version to 0.20.0

--- src/NomadIIS/PluginInfo.cs
+++ src/NomadIIS/PluginInfo.cs
@@ -3,5 +3,5 @@
 internal static class PluginInfo
 {
 	public static readonly string Name = "iis";
-	public static readonly string Version = "0.19.0";
+	public static readonly string Version = "0.20.0";
 }
